ec772e540c6fde267ac011a5a7b7410c0411d001
Sagar Kaushal <sagarkaushal74@gmail.com>
2016-08-29 15:28:21 +0500

    Modified factorial.cpp

--- factorial.cpp
+++ factorial.cpp
@@ -1,8 +1,14 @@
 #include "functions.h"
 
 int factorial(int n){
+	while(n<0)
+	{
+		printf("Re-enter a password");
+		scanf("%d",&n);
+	}
     if(n!=1){
 	return(n * factorial(n-1));
     }
     else return 1;
+
 }
