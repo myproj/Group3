a78823755411a014d032ca67e25bc9465c19f1c9
Rahul Singh <b14121@students.iitmandi.ac.in>
2016-08-29 03:32:31 -0700

    Modified factorial.cpp

--- factorial.cpp
+++ factorial.cpp
@@ -1,6 +1,13 @@
 #include "functions.h"
 
-int factorial(int n){
+int factorial(int n)
+{
+    do
+    {
+	printf("Enter a positive number : ");
+	scanf("%d",&n);
+    }while(n<0);
+
     if(n!=1){
 	return(n * factorial(n-1));
     }
