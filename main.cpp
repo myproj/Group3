1a74b44850053141f28d0622d653f6b0963bd995
Purushottam <purushottam014@gmail.com>
2016-09-05 13:43:19 +0530

    Makefile added

--- main.cpp
+++ main.cpp
@@ -4,6 +4,6 @@ using namespace std;
 int main(){
     print_hello();
     printf("\n");
-    printf("The factorial of 5 is %d\n",factorial(5));
+    printf("The factorial of -5 is %d\n",factorial(-5));
     return 0;
 }
